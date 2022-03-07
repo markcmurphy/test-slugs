1ac982ce392c21c95f46a9233da8b09c0381686c
Mark <mark@murphymark.me>
2022-03-07 17:19:41 -0600

    restructure

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { SignupForm } from './component/SignupForm.jsx';
+import { SignupForm } from '../component/SignupForm.jsx';
 
 export default function Home() {
   return (
